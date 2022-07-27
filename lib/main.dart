d6cf7f3e089c36a28513a2526a6cbf8f52ebe3ca
Cirrus CI <support@cirruslabs.org>
2022-07-27 22:44:54 +0000

    teste

--- lib/main.dart
+++ lib/main.dart
@@ -1,115 +1,116 @@
+import 'dart:math';
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
-  // This widget is the root of your application.
+class MyApp extends StatefulWidget {
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
+  _MyAppState createState() => _MyAppState();
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
+class _MyAppState extends State<MyApp> {
+  double iter = 0.0;
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+  // Choose a random color for this iteration
+  var colors = List.generate(20, (index) => ColorList[Random().nextInt(ColorList.length)]);
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+  void initState() {
+    super.initState();
+    Future.delayed(Duration(seconds: 1)).then((value) async {
+      for (int i = 0; i < 2000000; i++) {
+        setState(() {
+          iter = iter + 0.00001;
+        });
+        await Future.delayed(Duration(milliseconds: 50));
+      }
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+      home: Scaffold(
+        body: Container(
+          child: CustomPaint(
+            painter: DemoPainter(iter, colors),
+            child: Container(),
+          ),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
+}
+
+class DemoPainter extends CustomPainter {
+  double iter;
+  List<ColorInfo> colors;
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    renderDrawing(canvas, size);
+  }
+
+  void renderDrawing(Canvas canvas, Size size) {
+    canvas.drawPaint(Paint()..color = Colors.black87);
+
+    renderStructure2(canvas, size, iter, colors, 9);
+  }
+
+
+
+  void renderStructure2(Canvas canvas, Size size, double iter, List<ColorInfo> colors, int totalIter) {
+    for (int j = 0; j < totalIter; j++) {
+      double distance = 0.0;
+      double tempIter = totalIter - j + iter;
+
+      // We draw a LOT of points
+      for (double i = 0; i < 30; i = i + 0.01) {
+        canvas.drawCircle(
+          Offset(
+            (size.width / 2) + (distance * cos(distance) * sin(distance) * atan(distance)),
+            (size.height / 2) + (distance * cos(distance) * sin(distance) * tan(distance)),
+          ),
+
+          // Change this for changing radius in different iterations
+          2.0 - (0.6 * (totalIter - tempIter)),
+
+          // Well... color
+          Paint()..color = colors[j].color,
+        );
+
+        // Change the "0.1" for varying point distances
+        distance = distance + (0.2 + (0.4 * totalIter - tempIter));
+      }
+    }
+  }
+
+  @override
+  bool shouldRepaint(DemoPainter oldDelegate) {
+    return iter != oldDelegate.iter;
+  }
+
+  DemoPainter(this.iter, this.colors);
+}
+
+// This could have been a list of colors, I just picked the list off another project of mine
+var ColorList = [
+  ColorInfo("black", Colors.black, Colors.black.toString()),
+  ColorInfo("red", Colors.red, Colors.red[500].toString()),
+  ColorInfo("green", Colors.green, Colors.green[500].toString()),
+];
+
+class ColorInfo {
+  String name;
+  MaterialColor color;
+  String hex;
+
+  ColorInfo(this.name, this.color, this.hex);
 }
